0a535923171a5e12a8ff2798f14a92f9dba4560f
chenmichael2 <chenmichael2012@gmail.com>
2022-04-20 21:54:21 -0700

    adding the fragment

--- nextjs-course/pages/news/index.js
+++ nextjs-course/pages/news/index.js
@@ -1,7 +1,12 @@
 // our-domain.com/news
+import { Fragment } from 'react';
 
 function NewsPage() {
-    return <h1>The News Page</h1>
+    return
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul></ul>
+        </Fragment>
 }
 
 export default NewsPage;
