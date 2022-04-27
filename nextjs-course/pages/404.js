ee7f9adb6c024844f633e9df8c7c1b498b96b97d
chenmichael2 <chenmichael2012@gmail.com>
2022-04-27 15:42:59 -0700

    adding teh para and comment"

--- nextjs-course/pages/404.js
+++ nextjs-course/pages/404.js
@@ -1,10 +1,11 @@
-// 
+// for any routes that dont exist
 
 function NotFound() {
     return (
         <div className="not-found">
             <h1>Oooops....</h1>
             <h2>That page cannot be found.</h2>
+            <p>Go back to home</p>
         </div>
     )
 }
