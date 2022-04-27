1ed620f6429a40e8e495267ecae872e9cc6799e7
chenmichael2 <chenmichael2012@gmail.com>
2022-04-27 15:46:27 -0700

    making the link have anchor

--- nextjs-course/pages/404.js
+++ nextjs-course/pages/404.js
@@ -6,7 +6,9 @@ function NotFound() {
         <div className="not-found">
             <h1>Oooops....</h1>
             <h2>That page cannot be found.</h2>
-            <Link href="/"><p>Go back to home</p></Link>
+            <Link href="/"><a><p>Go back to home</p></a></Link>
         </div>
     )
 }
+
+export default NotFound;
