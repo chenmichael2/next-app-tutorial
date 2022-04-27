1bddf1313d895d3643f64b1d41a489c38349e020
chenmichael2 <chenmichael2012@gmail.com>
2022-04-27 15:56:10 -0700

    done with 404

--- nextjs-course/pages/404.js
+++ nextjs-course/pages/404.js
@@ -10,7 +10,8 @@ function NotFound() {
             // router.go(-1); go back and +1 goes foward
             router.push('/'); //redirect them to home page
         }, 3000)
-    }, [])
+    }, []);
+
     return (
         <div className="not-found">
             <h1>Oooops....</h1>
