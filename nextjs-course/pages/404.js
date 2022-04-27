e0ae90d6aa722e06e8914c20939c288be79e7d90
chenmichael2 <chenmichael2012@gmail.com>
2022-04-27 15:43:53 -0700

    adding the href

--- nextjs-course/pages/404.js
+++ nextjs-course/pages/404.js
@@ -6,7 +6,7 @@ function NotFound() {
         <div className="not-found">
             <h1>Oooops....</h1>
             <h2>That page cannot be found.</h2>
-            <Link><p>Go back to home</p></Link>
+            <Link href="/"><p>Go back to home</p></Link>
         </div>
     )
 }
